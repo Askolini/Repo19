145855679ffb2d5a57faa476652a8ec5b1c69e0e
Askolini <asmircc@gmail.com>
2021-10-01 23:58:05 +0200

    Destructuring

---
+++ ex04/src/index.js
@@ -0,0 +1,17 @@
+// src/index.js
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, Padawans!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
